c18ce74577a32c5ea9e0eb83f0ac9c20a7bacaeb
Hai Pham <haipham@Hais-MacBook-Pro.local>
2018-02-18 16:17:09 +0800

    refactor

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,31 @@
+const rp = require('chat-rp');
+
 const login = require('../apis/login');
 const register = require('../apis/register');
 const verify = require('../apis/verify');
 
-const ResponseFactory = require('../factories/response.factory');
-
 const routes = [{
   method: 'get',
   path: '/api/health-check',
-  func: (req, res) => ResponseFactory.ok(res, 'ok')
+  func: (req, res) => rp.ok(res, 'ok')
 }, {
   method: 'post',
   path: '/api/auth/login',
   func: (req, res) =>
     login(req.body)
-      .then(token => ResponseFactory.ok(res, token))
-      .catch((error) => ResponseFactory.error(res, error))
+      .then(token => rp.ok(res, token))
+      .catch((error) => rp.error(res, error))
 }, {
   method: 'post',
   path: '/api/auth/register',
   func: (req, res) =>
     register(req.body)
-      .then(token => ResponseFactory.ok(res, 'ok'))
-      .catch((error) => ResponseFactory.error(res, error))
+      .then(token => rp.ok(res, 'ok'))
+      .catch((error) => rp.error(res, error))
 }, {
   method: 'get',
   path: '/api/auth/verify/:token/:username',
-  func: (req, res) => ResponseFactory.ok(
+  func: (req, res) => rp.ok(
       res,
       verify({
         token: req.params.token,
